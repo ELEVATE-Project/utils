5108b7b19156333458a4b88b150273ae91aaddc8
rakeshSgr <rakesh.k@pacewisdom.com>
2025-01-23 13:29:56 +0530

    sunbird package fix

---
+++ sunbird-mentoring/helpers/userKafkaManager.js
@@ -0,0 +1,45 @@
+const { post } = require('../utils/requester')
+
+const kafkaManager = async (kafkaPackage, environmentVariables) => {
+	try {
+		const { ElevateKafka } = kafkaPackage
+
+		const kafka = new ElevateKafka(
+			environmentVariables.SUNBIRD_USER_KAFKA_BROKERS,
+			environmentVariables.SUNBIRD_USER_KAFKA_BROKERS,
+			{
+				packageName: 'sunbird-user',
+			}
+		)
+
+		const topics = [environmentVariables.SUNBIRD_USER_USER_UPDATE_KAFKA_TOPIC]
+		const consumer = await kafka.createConsumer(environmentVariables.SUNBIRD_USER_KAFKA_GROUP_ID, topics)
+		await kafka.runConsumer(consumer, async (topic, message) => {
+			if (topic == environmentVariables.SUNBIRD_USER_USER_UPDATE_KAFKA_TOPIC) {
+
+                if(process.env.DEBUG_MODE == "true"){
+					console.log(" user kafka -------------------- message",message.value);
+				}
+
+                let body = (JSON.parse(message.value));
+				if(process.env.DEBUG_MODE == "true"){
+					console.log("  user kafka -------------------- body ",body);
+				}
+
+				post(
+					environmentVariables.MENTORING_SERVICE_BASE_URL,
+					environmentVariables.MENTORING_SERVICE_USER_UPDATE_ROUTE,
+					JSON.parse(message.value),
+					{
+						INTERNAL_ACCESS_TOKEN: environmentVariables.MENTORING_INTERNAL_ACCESS_TOKEN,
+					}
+				)
+			}
+		})
+	} catch (error) {
+		console.log(error)
+		throw error
+	}
+}
+
+module.exports = kafkaManager
