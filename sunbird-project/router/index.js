60a2d8dc5c59d1c7a3fc566523627170d2f83995
VISHNUDAS <95604247+VISHNUDAS-tunerlabs@users.noreply.github.com>
2025-02-06 12:19:51 +0530

    Revert "sunbird-project package changes"

--- sunbird-project/router/index.js
+++ sunbird-project/router/index.js
@@ -1,13 +1,9 @@
 const { passThroughRequester } = require('../utils/requester')
 const { orchestrationHandler } = require('../controllers/orchestrationController')
-const { customHandler } = require('../controllers/customHandler')
-
 const packageRouter = async (req, res, responses) => {
-	let response
-	if (req.orchestrated) response = await orchestrationHandler(req, res, responses)
-	else if (req.requiresCustomHandling) {
-		response = await customHandler(req, res)
-	} else response = await passThroughRequester(req, res)
+	const response = req.orchestrated
+		? await orchestrationHandler(req, res, responses)
+		: await passThroughRequester(req, res)
 	return response
 }
 
