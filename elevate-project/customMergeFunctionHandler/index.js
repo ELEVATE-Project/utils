d005e6a12ae54e98b82ea92ba45a1926243c8114
borkarsaish65 <borkarsaish65@gmail.com>
2025-07-30 12:34:57 +0530

    feat:savepoint

--- elevate-project/customMergeFunctionHandler/index.js
+++ elevate-project/customMergeFunctionHandler/index.js
@@ -1,6 +1,5 @@
 const projectController = require('../controllers/project')
 const mergeHandler = async (result, functionName, packages) => {
-    console.log({result, functionName, packages})
 	return await projectController[functionName](result)
 }
 module.exports = mergeHandler
